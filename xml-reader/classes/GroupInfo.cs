2afd563ca07cfd5353ab67d44a04565e0db1dd9a
yunna <matveevayunna@gmail.com>
2026-06-18 16:46:07 +0300

    add: records for entities

---
+++ xml-reader/classes/GroupInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xml_reader.classes
+{
+    public record GroupInfo(
+    string Id,          // id_group
+    string Name         // group
+    );
+}
---
+++ xml-reader/classes/Teacher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xml_reader.classes
+{
+    public record Teacher(
+    string Id,          // id_prep
+    string FullName,    // prep
+    string Department   // chair 
+    );
+}
